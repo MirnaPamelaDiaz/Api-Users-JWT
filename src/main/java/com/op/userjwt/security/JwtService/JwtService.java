b9af038645230a5c63185beee3fb2c08e229fbd7
MirnaPamelaDiaz <pamela_diaz_24@outlook.com>
2023-01-26 10:29:01 -0300

    Add se agregan metodos a la clase JwtService Se agregan metodos de filtrado para el uso de JWT

--- src/main/java/com/op/userjwt/security/JwtService/JwtService.java
+++ src/main/java/com/op/userjwt/security/JwtService/JwtService.java
@@ -2,22 +2,64 @@ package com.op.userjwt.security.JwtService;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY="24432646294A404D635166546A576E5A7234753778214125442A472D4B615064";
+    private static final String SECRET_KEY = "24432646294A404D635166546A576E5A7234753778214125442A472D4B615064";
 
-    public String extractUsername(String jwt) {
-        return null;
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
     }
 
-    private Claims extractAllClaims(String token){
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims CLAIMS = extractAllClaims(token);
+        return claimsResolver.apply(CLAIMS);
+    }
+
+    public String generatetoken(UserDetails userDetails) {
+        return generatetoken(new HashMap<>(), userDetails);
+    }
+
+    public String generatetoken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 + 60 + 24))
+                .signWith(getSingKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String USERNAME = extractUsername(token);
+        return (USERNAME.equals(userDetails.getUsername()))&& !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token){
+        return false;
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
                 .setSigningKey(getSingKey())
@@ -28,5 +70,6 @@ public class JwtService {
 
     private Key getSingKey() {
         byte[] KeyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(KeyBytes);    }
+        return Keys.hmacShaKeyFor(KeyBytes);
+    }
 }

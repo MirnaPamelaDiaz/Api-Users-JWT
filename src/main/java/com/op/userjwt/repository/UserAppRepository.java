9bfde9d41f3e0dcff4a3593c3290efa1ac28387d
MirnaPamelaDiaz <pamela_diaz_24@outlook.com>
2023-01-26 02:29:58 -0300

    Add se agrega anotacion en clase UserAppRepository Se agrega anotacion @Repository Se agrega el metodo getsingKey en la clase JwtService

--- src/main/java/com/op/userjwt/repository/UserAppRepository.java
+++ src/main/java/com/op/userjwt/repository/UserAppRepository.java
@@ -2,9 +2,10 @@ package com.op.userjwt.repository;
 
 import com.op.userjwt.entity.UserApp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserAppRepository extends JpaRepository<UserApp, Integer> {
     Optional<UserApp> findByEmail(String email);
 }
--- src/main/java/com/op/userjwt/security/JwtService/JwtService.java
+++ src/main/java/com/op/userjwt/security/JwtService/JwtService.java
@@ -1,11 +1,32 @@
 package com.op.userjwt.security.JwtService;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY="24432646294A404D635166546A576E5A7234753778214125442A472D4B615064";
+
     public String extractUsername(String jwt) {
         return null;
     }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSingKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSingKey() {
+        byte[] KeyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(KeyBytes);    }
 }

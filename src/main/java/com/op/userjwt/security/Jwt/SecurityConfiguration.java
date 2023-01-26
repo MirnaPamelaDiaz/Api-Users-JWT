51c0b08e92f0882ff3bbe1a7a8516f48a77dca3d
MirnaPamelaDiaz <pamela_diaz_24@outlook.com>
2023-01-26 11:11:28 -0300

    Add se crea la clase SecurityConfiguration

---
+++ src/main/java/com/op/userjwt/security/Jwt/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.op.userjwt.security.Jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final JwtAuthenticationFilter JWT_AUTH_FILTER;
+    private final AuthenticationProvider AUTH_PROVIDER
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and()
+                .authenticationProvider(AUTH_PROVIDER)
+                .addFilterBefore(JWT_AUTH_FILTER, UsernamePasswordAuthenticationFilter.class)
+        return http.build();
+    }
+}
